e39512427203097893b0a5ee98031e9cd9fefc66
WesPereira <wesleyalmeida@usp.br>
2024-04-10 18:22:47 -0300

    Add largest num ex

--- toolbox/week3/largest_number.py
+++ toolbox/week3/largest_number.py
@@ -1,13 +1,27 @@
 from itertools import permutations
 
 
+def to_same_size(num_str, size):
+    while len(num_str) < size:
+        num_str += num_str[0]
+    return num_str
+
 def largest_number_naive(numbers):
     numbers = list(map(str, numbers))
 
-    largest = 0
+    max_size = -1
+    for num in numbers:
+        if len(num) > max_size:
+            max_size = len(num)
 
-    for permutation in permutations(numbers):
-        largest = max(largest, int("".join(permutation)))
+    new_nums = [(to_same_size(num, max_size), num) for num in numbers]
+
+    sorted_nums = sorted(new_nums, key=lambda x: (int(x[0]), 1- len(x[1])), reverse=True)
+
+    largest = ""
+
+    for _, num in sorted_nums:
+        largest += num
 
     return largest
 
